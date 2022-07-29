64c0b8e05fd6c50d2c0da020ceb2579c8d13b5a7
21018157 <21018157@myrp.edu.sg>
2022-07-29 16:29:41 +0800

    second page

--- app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/DBHelper.java
+++ app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/DBHelper.java
@@ -1,4 +1,51 @@
 package sg.edu.rp.c346.id21018157.mymovies;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "mymovies.db";
+    private static final int DATABASE_VERSION = 2;
+    private static final String TABLE_MOVIES = "movies";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_GENRE = "genre";
+    private static final String COLUMN_YEAR = "year";
+    private static final String COLUMN_RATING = "rating";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String createNoteTableSql = "CREATE TABLE " + TABLE_MOVIES + "("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_TITLE + " TEXT ," + COLUMN_GENRE + " TEXT ,"
+                    + COLUMN_YEAR + " INTEGER ," + COLUMN_RATING + " INTEGER ) ";
+            db.execSQL(createNoteTableSql);
+            Log.i("info", "created tables");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("ALTER TABLE " + TABLE_MOVIES + " ADD COLUMN  module_name TEXT ");
+        }
+
+        public long insertMovie(String movie, String genre, int year, String rating) {
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_TITLE, title);
+            values.put(COLUMN_GENRE, genre);
+            values.put(COLUMN_YEAR, year);
+            values.put(COLUMN_RATING, rating);
+            long result = db.insert(TABLE_MOVIES, null, values);
+            Log.d("DBHelper", "Insert failed");
+            db.close();
+             //id returned, shouldn’t be -1
+            return result;
+        }
+    }
 }
--- app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/ShowMovies.java
+++ app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/ShowMovies.java
@@ -2,13 +2,61 @@ package sg.edu.rp.c346.id21018157.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShowMovies extends AppCompatActivity {
+
+    Button btnShow;
+    ListView lv;
+    ArrayList<Movie> al;
+    ArrayAdapter<Movie> aa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_movies);
+
+        btnShow = findViewById(R.id.btnShow);
+        lv = findViewById(R.id.lv);
+
+        al = new ArrayList<Movie>();
+        aa = new CustomAdapter(this, R.layout.row, al);
+        lv.setAdapter(aa);
+
+        Intent i = getIntent();
+        data = (Movie) i.getSerializableExtra("data");
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper db = new DBHelper(ShowMovies.this);
+                al.clear();
+                al.addAll(db.getPG());
+                aa.notifyDataSetChanged();
+
+                db.close();
+            }
+        });
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int
+                    position, long identity) {
+                Movie data = al.get(position);
+                Intent i = new Intent(ShowMovies.this,
+                        ModifyMovies.class);
+                i.putExtra("data", data);
+                startActivity(i);
+            }
+        });
     }
 }
