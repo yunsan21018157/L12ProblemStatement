01cae43c4ef9c76aee5397e47626f50d36b9fe28
AJ-VICTOR <21008455@myrp.edu.sg>
2022-07-29 16:22:38 +0800

    HI

--- app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/MainActivity.java
@@ -3,12 +3,62 @@ package sg.edu.rp.c346.id21018157.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spin;
+    String rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spin.findViewById(R.id.spinner);
+
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        rating = "G";
+                        break;
+
+                    case 1:
+                        rating = "PG";
+                        break;
+
+                    case 2:
+                        rating = "PG13";
+                        break;
+
+                    case 3:
+                        rating = "NC16";
+                        break;
+
+                    case 4:
+                        rating = "M18";
+                        break;
+
+                    case 5:
+                        rating = "R21";
+                        break;
+
+                    default:
+                        rating = " ";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
--- app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/Movie.java
+++ app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/Movie.java
@@ -1,4 +1,57 @@
 package sg.edu.rp.c346.id21018157.mymovies;
 
 public class Movie {
+    private int id;
+    private String name;
+    private String genre;
+    private int year;
+    private char rating;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Movie(int id, String name, String genre, int year, char rating) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public char getRating() {
+        return rating;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setRating(char rating) {
+        this.rating = rating;
+    }
 }
